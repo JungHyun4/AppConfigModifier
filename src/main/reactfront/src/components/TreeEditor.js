e0854ed9efece32c6a19a48f97826c344e659f35
JungHyun4 <55117014+JungHyun4@users.noreply.github.com>
2023-02-13 15:57:03 +0900

    0213 updata
    일반 메뉴는 기능구현( 업데이트) 끝
    인터페이스는 뼈대만 마련.

--- src/main/reactfront/src/components/TreeEditor.js
+++ src/main/reactfront/src/components/TreeEditor.js
@@ -6,134 +6,149 @@ import JsYaml from "js-yaml";
 import {head} from "axios";
 import {map} from "react-bootstrap/ElementChildren";
 
-    const TreeEditor =  ({AppName ,  Content , Color}) => {
+const TreeEditor =  ({AppName ,  Content , Color}) => {
 
-        // 필요한 데이터셋 구성
-        const data = yaml.load(Content) // 원본데이터
-        const topHeaders = Object.keys(data) // 최상단 헤더
-        const interfaceHeaders = Object.keys(data["interface"]); // 인터페이스 헤더
-        const scheduleHeaders = Object.keys(data["schedule"]); // 스케줄 헤더
-        const [visible, SetVisible] = useState(false);
-        const [Change, SetChange] = useState("a");
-        const [Current, SetCurrent] = useState("");
-        const [List,SetList] = useState(new Map([]));
-        const component = new Map();
+    // 필요한 데이터셋 구성
+    const data = yaml.load(Content) // 원본데이터
+    const final = JSON.parse(JSON.stringify(data))
 
 
-        const [c,SetC] = useState();
-        const TreeModifier = (data, key) => {
-            const headers = Object.keys(data)
-            for (let i = 0 ; i < headers.length; i++){
-                if (typeof data != "object"){
-                    component.set(key, data)
-                }
-                else if (Array.isArray(data)){
-                    component.set(key+i.toString(),data[i])
-                }
+    const topHeaders = Object.keys(data) // 최상단 헤더
+    const interfaceHeaders = Object.keys(data["interface"]); // 인터페이스 헤더
+    const scheduleHeaders = Object.keys(data["schedule"]); // 스케줄 헤더
+    const [visible, SetVisible] = useState(false);
+    const [Change, SetChange] = useState("a");
+    const [Current, SetCurrent] = useState("");
+    const [List,SetList] = useState(final);
+    const [datadata,SetDataData] = useState(final);
+    const component = new Map();
 
-                else{
-                    TreeModifier(data[headers[i]],headers[i])
-                }
+
+
+    const [c,SetC] = useState();
+    const TreeModifier = (data, key) => {
+        const headers = Object.keys(data)
+        for (let i = 0 ; i < headers.length; i++){
+            if (typeof data != "object"){
+                component.set(key, data)
+            }
+            else if (Array.isArray(data)){
+                component.set(key+i.toString(),data[i])
+            }
+            else if (data == null){
+                console.log("null")
+                component.set(key,"")
+            }
+
+            else{
+                TreeModifier(data[headers[i]],headers[i])
             }
         }
-        const InterfaceMenu = () => {
-            const list = interfaceHeaders.map((name) => {
-                    return <li key={name} className={"interface"}
-                        onClick = {(e)=>{
-                            component.clear();
-                            console.log(data["interface"][name])
-                            console.log("인터페이스 하위메뉴 클릭됨");
-                        }
-                        }
-                    >{"-" + name}</li>
+    }
+    const InterfaceMenu = () => {
+        const list = interfaceHeaders.map((name) => {
+                return <li key={name} className={"interface"}
+                           onClick = {(e)=>{
+                               component.clear();
+                               SetCurrent(name)
+                               const data1 = data["interface"]
+                               TreeModifier(data1[name],name)
+                               SetList(component)
+                               Change =="a" ? SetChange("b") : SetChange("a")
+                               console.log(List)
+
+                           }
+                           }
+                >{"-" + name}</li>
+            }
+        )
+        return (<ul>
+            <li key={"interface"} onClick={ () => {
+                console.log("인터페이스 상위메뉴 클릭됨")
+                SetVisible(!visible)}}>interface</li>
+            {visible && <ul>{list}</ul>}
+
+        </ul>)
+    }
+    const Menu = (data) => {
+        const topHeaderList = topHeaders.filter(head => head !== "interface" && head !== "schedule").map((name) => {
+                return <li key={name} onClick={ () => {
+
+                    component.clear();
+                    SetCurrent(name)
+                    const data1 = data["data"]
+                    TreeModifier(data1[name],name)
+                    console.log("일반메뉴 클릭됨")
+                    SetList(component)
+                    Change =="a" ? SetChange("b") : SetChange("a")
                 }
-            )
-            return (<ul>
-                <li key={"interface"} onClick={ () => {
-                    console.log("인터페이스 상위메뉴 클릭됨")
-                    SetVisible(!visible)}}>interface</li>
-                {visible && <ul>{list}</ul>}
+                }>{name}</li>
+            }
+        )
+        return (<ul>{topHeaderList}</ul>)
+    }
 
-            </ul>)
-        }
-        const Menu = (data) => {
-            const topHeaderList = topHeaders.filter(head => head !== "interface" && head !== "schedule").map((name) => {
-                    return <li key={name} onClick={ () => {
+    const ScheduleMenu = () => {
+        return <ul><li key={"schedule"} onClick={() => {
+            visible == true? SetVisible(!visible) : SetVisible(false) // interface 서브메뉴 접기
+            console.log("스케쥴 메뉴 클릭됨")
+        }}>schedule</li></ul>
+    }
+    const ScheduleView = () => {
 
-                        component.clear();
-                        SetCurrent(name)
-                        const data1 = data["data"]
-                        TreeModifier(data1[name],name)
-                        console.log("일반메뉴 클릭됨")
-                        SetList(component)
-                        Change =="a" ? SetChange("b") : SetChange("a")
+    }
+
+    const Finish = (info) => {
+        // const real = JSON.parse(JSON.stringify(datadata))
+        const arr = Array.from(info["info"]);
+
+        console.log(arr)
+        const a = arr.map((sub) => {
+            if (Current == "alias"){
+                return <li><label>{sub[0]}</label><input defaultValue={sub[1]} onChange={
+                    (e) => {
+                        const real = JSON.parse(JSON.stringify(datadata))
+                        real[Current][sub[0].charAt(sub[0].length-1)*1] = e.target.value;
+                        SetDataData(real)
                     }
-                    }>{name}</li>
-                }
-            )
-            return (<ul>{topHeaderList}</ul>)
-        }
-
-        const ScheduleMenu = () => {
-            return <ul><li key={"schedule"} onClick={() => {
-                visible == true? SetVisible(!visible) : SetVisible(false) // interface 서브메뉴 접기
-                console.log("스케쥴 메뉴 클릭됨")
-            }}>schedule</li></ul>
-        }
-
-        const Finish = (info) => {
-            console.log(info)
-            const arr = Array.from(info["info"]);
-            console.log(data["alias"][0])
-            // console.log(Current)
-            // console.log(data[Current]["sender"])
-            const num = ["0","1","2","3","4","5","6","7","8","9"]
-
-            const a = arr.map((sub) => {
-                if (Current == "alias"){
-                    return <li><label>{sub[0]}</label><input defaultValue={sub[1]} onChange={
+                }/></li>
+            }
+            else if (Current in interfaceHeaders){
+                console.log("info")
+            }
+            else{
+                const nums = ["0","1","2","3","4","5","6","7","8","9"]
+                if (sub[0].charAt(sub[0].length-1) in nums){ // list인 경우
+                    return <li><label>{sub[0].substr(0,sub[0].length-1)}</label><input key={sub[0]} defaultValue={sub[1]} onChange={
                         (e) => {
-                          data[Current][sub[0].charAt(sub[0].length-1)*1] = e.target.value;
-                          console.log(data)
+                            const real = JSON.parse(JSON.stringify(datadata))
+                            real[Current][sub[0].substr(0,sub[0].length-1)] = e.target.value
+                            SetDataData(real)
                         }
                     }/></li>
                 }
                 else{
-                    const nums = ["0","1","2","3","4","5","6","7","8","9"]
-                    if (sub[0].charAt(sub[0].length-1) in nums){ // list인 경우
-                        return <li><label>{sub[0].substr(0,sub[0].length-1)}</label><input key={sub[0]} defaultValue={sub[1]} onChange={
-                            (e) => {
-                                data[Current][sub[0].substr(0,sub[0].length-1)] = e.target.value
-                                console.log(data)
-
-                            }
-                        }/></li>
-                    }
-                    else{
-                        return <li><label>{sub[0]}</label><input defaultValue={sub[1]} onChange={
-                            (e) => {
-                                data[Current][sub[0]] = e.target.value;
-                                console.log(data)
-                            }
-                        }/></li>
-                    }
+                    return <li><label>{sub[0]}</label><input defaultValue={sub[1]} onChange={
+                        (e) => {
+                            const real = JSON.parse(JSON.stringify(datadata))
+                            real[Current][sub[0]] = e.target.value;
+                            SetDataData(real)
+                        }
+                    }/></li>
                 }
-
-            })
-
-            return <ul>{a}</ul>
-        }
-
-
-        return (
+            }
+        })
+        return <ul>{a}</ul>
+    }
+    return (
         <div>
             <label style={ {color:Color}}  name="appName" className="AppName" >{AppName}</label>
 
             <div className="TreeContainer">
                 <div className="left">
-                        <Menu data={data}></Menu>
-                        <InterfaceMenu></InterfaceMenu>
-                        <ScheduleMenu ></ScheduleMenu>
+                    <Menu data={data}></Menu>
+                    <InterfaceMenu></InterfaceMenu>
+                    <ScheduleMenu ></ScheduleMenu>
                 </div>
 
                 <div className="right">
